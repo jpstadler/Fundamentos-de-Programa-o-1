f44ad248483cfb6496bcfd2c587ee703b3f6c636
devieirad <d-vieira@live.com>
2017-04-01 01:30:54 -0300

    Update Calendario.c

--- Calendario.c
+++ Calendario.c
@@ -2,16 +2,13 @@
 
 main()
 {
-    int ano, a, b, c, w, i,l=0, j=0, k=0, kp=0, mult, ordem,
-                           diaSemana, bissexto, sec, anoaux, diaaux,
-                           ordemMax,
-                           ehBissexto = 0; //Variável indicadora para ano bissexto
+    int ano, auxa, auxb, auxc,
+        l=0, j=0, k=0, kp=0,
+        diaSemana,
+        ehBissexto = 0; //Variável indicadora para ano bissexto
 
-    printf("Informe o ano");
+    printf("Informe o ano: ");
     scanf("%d", &ano);
-    anoaux=ano;
-    printf("%d\n",anoaux);
-
 
     //Verificação de ano bissexto
     if(ano%4==0 && ano%100!=0 || ano%400==0)
@@ -19,98 +16,51 @@ main()
 
     //dia da semana de 01/01/AAAA
     //1/1/1900 eh segunda
-    /*a = ano-1900;            //quantos anos passaram desde 1900
-    b = a/4;                //quantos as bissextos;
-    c = 365*(a-b) + 366*b; //qunatidade de dias entre a 01/01/1900 e a data;
-    diaSemana = c%7;
-    printf("\n%d\n%d\n%d\n%d\n",diaSemana,a,b,c);*/
+    auxa = ano-1900;            //quantos anos passaram desde 1900
+    auxb = auxa/4;                //quantos as bissextos;
+    auxc = (365*(auxa-auxb) + 366*auxb)+1; //qunatidade de dias entre a 01/01/1900 e a data;
+    diaSemana = auxc%7;
 
-
-    sec=0;
-    anoaux-=2000;
-    printf("%d\n",anoaux);
-    mult=7;
-
-    while(anoaux%mult>6)
-        mult+=7;
-
-    diaSemana=((anoaux-mult)+(anoaux/4))+sec;
-    mult=0;
-
-    while(mult<diaSemana)
-        mult+=7;
-
-    printf("%d\n",diaSemana);
-    if(diaSemana>7)
-        diaSemana-=mult;
-
-    printf("%d\n",diaSemana);
-
-    switch (diaSemana)
+    if(!ehBissexto)
     {
-    case 0:
-        printf("Domingo\n");
-        break;
-    case 1:
-        printf("Segunda-feira\n");
-        break;
-    case 2:
-        printf("Terça-feira\n");
-        break;
-    case 3:
-        printf("Quarta-feira\n");
-        break;
-    case 4:
-        printf("Quinta-feira\n");
-        break;
-    case 5:
-        printf("Sexta-feira\n");
-        break;
-    case 6:
-        printf("Sábado\n");
-        break;
-    }
-
-    diaSemana=4; /*AINDA NÃO CONSEGUI ACERTAR O CÁLCULO DO DIA DA SEMANA!!! POR ISSO FORJEI ESSE RESULTADO.*/
-
-    /*if(!ehBissexto)
-    {*/
-    printf("\t\tJANEIRO de %d\nDOM\tSEG\tTER\tQUA\tQUI\tSEX\tSAB\n", ano);
-    for(j=0; j<diaSemana; j++)
-        printf(" \t");
-    /*A primeira semana tem um comportamento diferente das outras,
-    é impossível alcançar dois resultados diferentes com a mesma entrada,
-    por isso criei um laço só pra ela. Isso inclui os espaçamento, por isso
-    criei 'kp'(k da primeira semana) pra não desorganizar o resto do mes.*/
-    while(l<7-diaSemana)
-    {
-        kp++;
-        printf("%d\t",kp);
-        l++;
-    }
-
-    /*Formatação das semanas comuns*/
-    printf("\n");
-    for(k=kp; k<=31; k+0)
-    {
-        for(j=0; j<=6; j++)
+        printf("\n\n\t\tJANEIRO de %d\nDOM\tSEG\tTER\tQUA\tQUI\tSEX\tSAB\n", ano);
+        for(j=0; j<diaSemana; j++)
+            printf(" \t");
+        /*A primeira semana tem um comportamento diferente das outras,
+        é impossível alcançar dois resultados diferentes com a mesma entrada,
+        por isso criei um laço só pra ela. Isso inclui os espaçamento, por isso
+        criei 'kp'(k da primeira semana) pra não desorganizar o resto do mes.*/
+        for(l=0; l<7-diaSemana; l++)
         {
-            /*esse laço foi necessario porque eu precisei de uma quebra de linha fora de um laço,
-            para que ele não pulasse uma linha apos cada dia*/
-            k++;
-            if(k>31)                 //termino de mes ocorre apenas uma vez, por isso vem antes
-                break;
-            else
-                printf("%d\t",k);
+            kp++;
+            printf("%d\t",kp);
         }
-        if(j>6)                    //quebra de linha da semana comum
-            printf("\n");
+
+        /*Formatação das semanas comuns*/
+        printf("\n");
+        for(k=kp; k<=31; k+0)
+        {
+            for(j=0; j<=6; j++)
+            {
+                /*esse laço foi necessario porque eu precisei de uma quebra de linha fora de um laço,
+                para que ele não pulasse uma linha apos cada dia*/
+                k++;
+                if(k>31)                 //termino de mes ocorre apenas uma vez, por isso vem antes
+                    break;
+                else
+                    printf("%d\t",k);
+            }
+            if(j>6)                    //quebra de linha da semana comum
+                printf("\n");
+        }
+
+
+        /*for(i=2; i<=12; i++){
+            printf("\t\t %d de %d\nDOM\tSEG\tTER\tQUA\tQUI\tSEX\tSAB\n", i,ano);
+        }*/
+
+        /*}*/
     }
-
-
-    /*for(i=2; i<=12; i++){
-        printf("\t\t %d de %d\nDOM\tSEG\tTER\tQUA\tQUI\tSEX\tSAB\n", i,ano);
-    }*/
-
-    /*}*/
+    else
+        printf("Eh Bissexto!!\n");
 }
